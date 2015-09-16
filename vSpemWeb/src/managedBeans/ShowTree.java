8c2e0c441a9c302c82f22679d502cccd3fa9d13a
fabianarol <fabianarol05@gmail.com>
2015-09-16 18:38:10 -0300

    Modelo adaptado funcionando

--- vSpemWeb/src/managedBeans/ShowTree.java
+++ vSpemWeb/src/managedBeans/ShowTree.java
@@ -1,21 +1,16 @@
 package managedBeans;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
-
-import logica.XMIParser;
 
 import org.primefaces.model.CheckboxTreeNode;
 import org.primefaces.model.TreeNode;
 
-import dominio.Struct;
+import dominio.Document;
 
 @ManagedBean(name="showtree")
 public class ShowTree implements Serializable {
@@ -24,7 +19,7 @@ public class ShowTree implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private TreeNode treeAdaptado;
+	private TreeNode treeAdaptado = null;
 	
 	
 	public TreeNode getTreeAdaptado() {
@@ -47,37 +42,36 @@ public class ShowTree implements Serializable {
 		HttpSession session = (HttpSession) context.getExternalContext().getSession(false);
 		SelectionView sv =(SelectionView) session.getAttribute("SelectionView");
 		if (sv != null){
-			System.out.println("Bean selectionview es null");
+			System.out.println("Bean selectionview no es null");
 			sv.displaySelectedMultiple(selecionados);
 		}
-		treeAdaptado = tree;
-		if (!(treeAdaptado == null)){
-		System.out.println("Hijos del nodo ################" + treeAdaptado.getChildCount());
-		List<TreeNode> nodosNivel2 = treeAdaptado.getChildren();
-		
-		for (int temp = 0; temp < nodosNivel2.size(); temp++){
-			TreeNode hijoNivel2 = nodosNivel2.get(temp);
-			TreeNode padreNivel1 = hijoNivel2.getParent();
-			List<TreeNode> nodosNivel3 = hijoNivel2.getChildren();
-			System.out.println("Hijos del nodo nivel 2 ################" + nodosNivel3.size());
-			for (int temp1 = 0;temp1 < nodosNivel3.size(); temp1++){
-				TreeNode hijoNivel3 = nodosNivel3.get(temp1);
-				TreeNode padreNivel2 = hijoNivel3.getParent();
-				System.out.println("Hijo 3 ################" + hijoNivel3.toString());
-				System.out.println("Hijo 3 ################" + hijoNivel3.isSelected());
-				//if (hijoNivel3.isSelectable()){
-				 if (hijoNivel3.isSelected()){
-					 hijoNivel3.setParent(padreNivel1);
-					 System.out.println("Padre es:" + hijoNivel3.getParent().toString());
-					 System.out.println("ESta seleccionadoooooo" + hijoNivel3.toString());
-					}
-				//}
-			}
-		}
+
+		if (tree != null){
 			
-	}
-		VistaBean vb =(VistaBean) session.getAttribute("VistaBean");
-		vb.setTreeAdaptado(treeAdaptado);
+			treeAdaptado =  new CheckboxTreeNode(new Document(((Document)tree.getData()).getElementID(),((Document)tree.getData()).getName(),"-", "Folder"),null);
+			treeAdaptado.setExpanded(true);
+			List<TreeNode> nodosNivel2 = tree.getChildren();
+			
+			for (int temp = 0; temp < nodosNivel2.size(); temp++){
+				TreeNode hijoNivel2 = nodosNivel2.get(temp);
+				List<TreeNode> nodosNivel3 = hijoNivel2.getChildren();
+				if(nodosNivel3.size()>0){
+					for (int temp1 = 0;temp1 < nodosNivel3.size(); temp1++){
+						TreeNode hijoNivel3 = nodosNivel3.get(temp1);
+						
+						 if (hijoNivel3.isSelected()){
+							 TreeNode nuevoHijoNivel3 = new CheckboxTreeNode(new Document(((Document)hijoNivel3.getData()).getElementID(),((Document)hijoNivel3.getData()).getName(),"-", "Folder"),treeAdaptado);
+							 nuevoHijoNivel3.setExpanded(true);
+							}
+					}
+				} else {
+					TreeNode nuevoHijoNivel2 = new CheckboxTreeNode(new Document(((Document)hijoNivel2.getData()).getElementID(),((Document)hijoNivel2.getData()).getName(),"-", "Folder"),treeAdaptado);
+					nuevoHijoNivel2.setExpanded(true);	
+				}
+			 }
+		  }
+		  VistaBean vb =(VistaBean) session.getAttribute("VistaBean");
+		  vb.setTreeAdaptado(treeAdaptado);
 		
 	}
 }
